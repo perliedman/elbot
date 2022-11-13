b19b47845dd2b962cb1de8623016513a179f0f3b
Per Liedman <per@liedman.net>
2022-11-13 22:26:22 +0100

    Output HTML

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,36 @@
 import { fetchPrices, getAreaPriceData, getMessage } from "./src/index.mjs";
+import data from "./test/data/221113.json" assert { type: "json" };
 
-const [,, areaName] = process.argv;
+const [, , areaName] = process.argv;
 
-const data = await fetchPrices();
+// const data = await fetchPrices();
 const areaPriceData = getAreaPriceData(data, areaName);
-console.log(getMessage(areaPriceData));
+console.log(`
+<html>
+  <head>
+    <style>
+      body {
+        background-color: #111128;
+        font-size: 48px;
+        color: #48486f;
+      }
+      ul {
+        list-style: none;
+        margin-top: 25vh;
+        text-align: center;
+        font-family: sans-serif;
+      }
+      li {
+        line-height: 2;
+      }
+    </style>
+  </head>
+  <body>
+    <ul>
+      ${getMessage(areaPriceData)
+        .split("\n")
+        .map((l) => `<li>${l}</li>`)
+        .join("\n")}
+    </ul>
+  </body>
+</html>`);
