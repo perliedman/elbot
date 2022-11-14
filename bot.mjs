41a5739a7adac14ecbb024191318ffe99039fa64
Per Liedman <per@liedman.net>
2022-11-14 21:53:04 +0100

    Remove unused

--- bot.mjs
+++ bot.mjs
@@ -1,13 +1,16 @@
-import { fetchPrices, getAreaPriceData, getMessage, sendStatus } from "./src/index.mjs";
+import {
+  fetchPrices,
+  getAreaPriceData,
+  getMessage,
+  sendStatus,
+} from "./src/index.mjs";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const [, , accessToken, areaName] = process.argv;
-
 const data = await fetchPrices(process.env.ENTSOE_TOKEN);
-const areaPriceData = getAreaPriceData(data, areaName);
-const message = getMessage(areaPriceData)
+const areaPriceData = getAreaPriceData(data);
+const message = getMessage(areaPriceData);
 console.log(`
 <html>
   <head>
@@ -39,5 +42,8 @@ console.log(`
   </body>
 </html>`);
 
-await sendStatus(message, process.env.MASTODON_ACCESS_TOKEN, process.env.MASTODON_API_URL)
-
+await sendStatus(
+  message,
+  process.env.MASTODON_ACCESS_TOKEN,
+  process.env.MASTODON_API_URL
+);
